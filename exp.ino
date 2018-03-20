e96c12c29f6b20c0961360729da0cc0c198222f9
Karthik Kasturi <karthikkasturi97@gmail.com>
2018-03-20 00:59:38 -0700

    Added static Arduino code.

---
+++ exp.ino
@@ -0,0 +1,10 @@
+void setup() {
+
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("Medical_IOT_PulseOximeter,John Doe,120,17.3971, 78.4903");
+delay(12000);
+}
